a1c53280a20b1375da08844039c5bbe7262bf7d4
Kyle Black (1000026735) <BLACK11@student.op.ac.nz>
2025-04-10 10:10:09 +1200

    feat: create moveable block script

---
+++ Assets/Scripts/InteractiveObjs/InteractiveBlock.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class InteractiveBlock : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private bool isColliding = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        moveDirection = (transform.position - other.transform.position);
+        moveDirection.y = 0f;
+        moveDirection.Normalize();
+
+        isColliding = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isColliding = false;
+        moveDirection = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (isColliding)
+        {
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+    }
+}
