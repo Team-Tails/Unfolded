22906c5bafc1eeacf639472390140af58bb6e088
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2025-05-15 18:19:40 +1200

    feat(collectable-stars): add   assets to the collectable star prefab

--- Assets/Scripts/CollectableStar.cs
+++ Assets/Scripts/CollectableStar.cs
@@ -1,12 +1,24 @@
+/*
+* got help from here https://www.youtube.com/watch?v=FjJJ_I9zqJo for the rotation
+*/
+
 using UnityEngine;
+
+
 
 public class CollectableStar : MonoBehaviour
 {
 
+    void Update()
+    {
+        //rotate star to face camera
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+    }
+
     void OnTriggerEnter(Collider other)
     {
 
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             GameManager.Instance.StarCount += 1;
             Destroy(gameObject);
